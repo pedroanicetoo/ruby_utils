5e7f94c90c69f94cea951202232d8c269852a1a2
pedroanicetoo <pedro0077@gmail.com>
2023-04-21 23:48:36 -0300

    finish implementation of persistent_list

--- data_structures/persistent_list.rb
+++ data_structures/persistent_list.rb
@@ -1,24 +1,11 @@
 require_relative 'linked_list'
 
-class Node
-  attr_accessor :data, :next
-
-  def initialize(data)
-    @data = data
-    @next = nil
-  end
-end
-
 class PersistentList
-  def self.empty
-    list = LinkedList.new
-    list.freeze
-  end
 
   def self.insert(list, data)
-    ret = self.copy list
-    ret.insert data
-    ret.freeze
+    readonly = self.copy list
+    readonly.insert data
+    readonly.freeze
   end
 
   def self.update(list, node, data)
@@ -29,6 +16,38 @@ class PersistentList
     remove_or_update_steps(list, node)
   end
 
+  def self.empty
+    list = LinkedList.new
+    list.freeze
+  end
+
+  def self.cat(lhs, rhs)
+    readonly = copy lhs
+    readonly.cat rhs
+    readonly.freeze
+  end
+
+  def self.len list
+    list&.length || 0
+  end
+
+  def self.find_first list &predicate
+    return unless list
+
+    list.find_first &predicate
+  end
+
+  def self.each list, &block
+    return unless list
+
+    list.each &block
+  end
+
+  def self.print list
+    return 'empty' unless list
+
+    list.print
+  end
 
   class << self
     private
@@ -58,3 +77,61 @@ class PersistentList
   end
 
 end
+
+
+=begin
+  USAGE EXAMPLES
+  pl = PersistentList
+  ll = LinkedList.new
+
+  #insert(list, data) => (readonly LinkedList)
+  pl.insert(ll, 1)
+  pl.insert(ll, 2)
+  pl.insert(ll, 3)
+  -> [1, 2, 3] (LinkedList data values)
+
+  #update(list, node, data) => (readonly LinkedList)
+  old_node = ll.head.next (# 2)
+  pl.update(ll, old_node, 5)
+  -> [1, 5, 3] (LinkedList data values)
+
+  #remove(list, node) => (readonly LinkedList)
+  node = ll.head (# 1)
+  pl.remove(ll, node)
+  -> [2, 3] (LinkedList data values)
+
+  #empty => (readonly LinkedList)
+  pl.empty
+  -> #<LinkedList:0x00007f332f3b3058 @head=nil, @length=0, @tail=nil>
+
+  #cat(left_list, right_list) => (readonly LinkedList)
+  ll = LinkedList.new
+  ll.insert 1
+  rl = LinkedList.new
+  rl.insert 2
+  pl.cat(ll, rl)
+  -> [1, 2] (LinkedList data values)
+
+  #len(list) => (Integer)
+  ll = LinkedList.new
+  pl.len(ll)
+  -> 0
+
+  #find_first(list) &predicate => (Node) | #each(list) &block => nil
+  ll = LinkedList.new
+  ll.insert 1
+  ll.insert 2
+  PersistentList.find_first(ll) { |data| data == 2 }
+  -> #<Node:0x00007f332f2db298 @data=2, @next=nil>
+  PersistentList.each(ll) {|node| puts 'match!' if  node.data == 2 }
+  -> match!
+  -> nil
+
+  #print => (string || readonly LinkedList)
+  ll = LinkedList.new
+  ll.insert 1
+  ll.insert 2
+  PersistentList.print(ll)
+  -> [1, 2] (LinkedList data values)
+
+=end
