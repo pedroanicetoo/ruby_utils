b96bb29f7c9934b0938acc356e0264a63980c8ab
pedroanicetoo <pedro0077@gmail.com>
2023-04-30 22:00:41 -0300

    starts implement btree

---
+++ data_structures/btree.rb
@@ -0,0 +1,53 @@
+class Node
+  attr_accessor :parent, :data, :left, :right
+
+  def initialize(parent, data)
+    @data = data
+    @parent = parent
+    @left = nil
+    @ight = nil
+  end
+end
+
+class Btree
+  attr_accessor :root, :size
+
+  def initialize(root)
+    @root = root
+    @size = 1
+  end
+
+  def insert_left(node, data)
+    return unless node
+
+    raise "Can't override nodes :(" if node.left
+    insert(:left, node, data)
+  end
+
+  def insert_right(node, data)
+    return unless node
+
+    raise "Can't override nodes :(" if node.right
+    insert(:right, node, data)
+  end
+
+  private
+
+  def insert(type, node, data)
+    @size += 1
+    node.instance_variable_set("@#{type.to_s}", Node.new(node, data))
+  end
+
+end
+
+=begin
+  USAGE EXAMPLES
+
+  root = Node.new(nil, 5)
+  b = Btree.new(root)
+  n1 = b.insert_left(root, 4)
+  n1_error = b.insert_left(root, 3) # raise an error
+  n2 = b.insert_right(root, 6)
+  n1_error = b.insert_right(root, 7) # raise an error
+
+=end
