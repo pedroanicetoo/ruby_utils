b62e018b10ec725bc9bf7a5b95394d09abb3eec7
pedroanicetoo <pedro0077@gmail.com>
2023-05-01 23:23:39 -0300

    Btree#remove_left#remove_right

--- data_structures/btree.rb
+++ data_structures/btree.rb
@@ -31,6 +31,28 @@ class Btree
     insert(:right, node, data)
   end
 
+  def remove_left(node)
+    return unless node&.left
+
+    remove_left(node.left)
+    remove_right(node.left)
+    node.left = nil
+    @size -= 1
+    self
+  end
+
+  def remove_right(node)
+    return unless node&.right
+
+    remove_left(node.right)
+    remove_right(node.right)
+    node.right = nil
+    @size -= 1
+    self
+  end
+
+
+
   private
 
   def insert(type, node, data)
@@ -45,9 +67,38 @@ end
 
   root = Node.new(nil, 5)
   b = Btree.new(root)
+
+  .insert_left => (self)
   n1 = b.insert_left(root, 4)
   n1_error = b.insert_left(root, 3) # raise an error
+
+  .insert_right => (self)
   n2 = b.insert_right(root, 6)
   n1_error = b.insert_right(root, 7) # raise an error
+
+  n1_2 = b.insert_left(n1, 3)
+  n2_2 = b.insert_right(n2, 7)
+
+     5
+    / \
+   4   6
+  /     \
+ 3       7
+
+  .remove_left => (self)
+  b.remove_left(n1_2.parent) # removing 3
+
+   5
+  / \
+ 4   6
+      \
+       7
+
+  .remove_right => (self)
+  b.remove_right(n2_2.parent) # removing 7
+
+   5
+  / \
+ 4   6
 
 =end
